a3a73ec19c499cd881f7a055a68995934cd735c7
Justin Ee <isthatjustin@gmail.com>
2023-07-01 23:00:26 +0800

    update to original code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
 import Budget from "./components/Budget";
-//import Login from "./components/Login"
+import Login from "./components/Login"
 import Insights from "./components/Insights"
 import "./App.css";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp"; 
-import AuthDetails from './components/AuthDetails'; 
-
 
 // import { BudgetsProvider } from "./api/budget";
 import { Route, Routes } from "react-router-dom";
@@ -14,13 +10,9 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <div className="App"> 
-        <SignUp />
-        <AuthDetails />
-      </div>
 
       <Routes>
-        <Route path="/" element={<SignIn />} />  {/* This is the default page */}
+        <Route path="/" element={<Login />} />  {/* This is the default page */}
         <Route path="/Budget" element={<Budget />} />
         <Route path="/Insights" element={<Insights />} />
       </Routes>
